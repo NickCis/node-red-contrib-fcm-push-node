5f5b98229c09741bf5df628086b4e77fbed0cbd3
Nicolas Cisco <ncis20@gmail.com>
2020-03-20 03:50:52 -0300

    Fixing typos

--- src/fcm-push.js
+++ src/fcm-push.js
@@ -17,21 +17,21 @@ module.exports = RED => {
         if (msg.payload.data)
           message.data = msg.payload.data;
         if (msg.payload.registration_ids)
-          message.data = msg.payload.registration_ids;
+          message.registration_ids = msg.payload.registration_ids;
         if (msg.payload.condition)
-          message.data = msg.payload.condition;
+          message.condition = msg.payload.condition;
         if (msg.payload.priority)
-          message.data = msg.payload.priority;
+          message.priority = msg.payload.priority;
         if ('content_available' in msg.payload)
-          message.data = msg.payload.content_available;
+          message.content_available = msg.payload.content_available;
         if ('mutable_content' in msg.payload)
-          message.data = msg.payload.mutable_content;
+          message.mutable_content = msg.payload.mutable_content;
         if (msg.payload.time_to_live)
-          message.data = msg.payload.time_to_live;
+          message.time_to_live = msg.payload.time_to_live;
         if (msg.payload.restricted_package_name)
-          message.data = msg.payload.restricted_package_name;   
+          message.restricted_package_name = msg.payload.restricted_package_name;   
         if ('dry_run' in msg.payload)
-          message.data = msg.payload.dry_run;
+          message.dry_run = msg.payload.dry_run;
 
         fcm.send(message)
           .then(response => {
